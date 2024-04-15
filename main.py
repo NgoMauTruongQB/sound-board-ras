94ebd9c9b0a2ae1ae151d88a678600035aa7b087
NgoMauTruongQB <truongngo2707@gmail.com>
2024-04-15 17:19:09 +0700

    update

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
-import pygame.mixer
-from time import sleep
+from pydub import AudioSegment
+from pydub.playback import play
 import RPi.GPIO as GPIO
+from time import sleep
 from sys import exit
+import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 GPIO.setup(24, GPIO.IN)
 GPIO.setup(25, GPIO.IN)
 
-pygame.mixer.init(48000, -16, 1, 1024)
+# Lấy đường dẫn thư mục hiện tại của script
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-sndA = pygame.mixer.Sound("sound1.wav")
-sndB = pygame.mixer.Sound("sound2.wav")
-sndC = pygame.mixer.Sound("sound3.wav")
+# Tạo các đường dẫn tuyệt đối đến các file âm thanh
+soundA = AudioSegment.from_file(os.path.join(script_dir, "sound1.mp3"))
+soundB = AudioSegment.from_file(os.path.join(script_dir, "sound2.mp3"))
+soundC = AudioSegment.from_file(os.path.join(script_dir, "sound3.mp3"))
 
-soundChannelA = pygame.mixer.Channel(1)
-soundChannelB = pygame.mixer.Channel(2)
-soundChannelC = pygame.mixer.Channel(3)
+print("Soundboard Ready.")
 
-print "Sampler Ready."
+current_playing = None
 
 while True:
-   try:
-      if (GPIO.input(23) == True):
-         soundChannelA.play(sndA)
-      if (GPIO.input(24) == True):
-         soundChannelB.play(sndB)
-      if (GPIO.input(25) == True):
-         soundChannelC.play(sndC)
-      sleep(.01)
-   except KeyboardInterrupt:
-      exit()
+    try:
+        if GPIO.input(23):
+            if current_playing != soundA:
+                if current_playing:
+                    play(soundA)
+                    current_playing = soundA
+        if GPIO.input(24):
+            if current_playing != soundB:
+                if current_playing:
+                    play(soundB)
+                    current_playing = soundB
+        if GPIO.input(25):
+            if current_playing != soundC:
+                if current_playing:
+                    play(soundC)
+                    current_playing = soundC
+        sleep(0.01)
+    except KeyboardInterrupt:
+        exit()
